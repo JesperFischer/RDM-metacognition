d832a1b1a72449ffbb928b8d74fd11ed2b207d81
jesperfischer <jesperfischer@outlook.dk>
2026-03-02 13:13:27 +0100

    scripts

--- Experimental analysis/Scripts/Load_data.R
+++ Experimental analysis/Scripts/Load_data.R
@@ -8,10 +8,8 @@ load_data = function(){
   df = data.frame()
   for(path in paths){
     print(path)
-    subjectID = strsplit(path, '/')[[1]][[7]]
+    subjectID = strsplit(path, '/')[[1]][[8]]
     
-    dq = read.csv(path)
-
     dd = read.csv(path) %>% mutate(X = NULL) %>% 
       mutate(ID = subjectID) %>% 
         mutate(resp = ifelse(resp == "down",0,1),
@@ -19,15 +17,20 @@ load_data = function(){
                D = ifelse(dots.direction == 270,-1,1),
                stim = ifelse(dots.direction == 270, -coherence,coherence)) %>%
       mutate(ACC = ifelse(resp == 1 & stim > 0, 1,
-                          ifelse(resp == 0 & stim < 0, 1,0))) %>%
+                          ifelse(resp == 0 & stim < 0, 1,0)))
       # filter(Trialtype == "Main") %>%
       # filter(SR_conf != "None") %>%
       # filter(scale == "conf") %>%
-      mutate(SR_conf = as.numeric(SR_conf)) %>%
+      
+      if ("sub" %in% names(dd)) {
+        names(dd)[names(dd) == "sub"] = "subject"
+      }
+      
+    dd = dd %>% mutate(SR_conf = as.numeric(SR_conf)) %>%
       rename(X = stim, RT = RTdec, Correct = cor, Y = resp, Confidence = SR_conf) %>% 
       select(X,Correct,Y,RT,Confidence,scale,D, coherence,Trialtype,
              interval,interTrial.interval,
-             subject,age,gender,ID,handedness)
+             subject,age,gender,ID)
     
   
   df = rbind(df,dd)
